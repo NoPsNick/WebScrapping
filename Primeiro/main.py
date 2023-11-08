270f94e49cea7a2209aee41eadf00b3ca30fd97e
Francisco Serafim <64261693+NoPsNick@users.noreply.github.com>
2023-11-08 15:23:53 -0300

    Finalizando o aplicativo geral, por fim adicionado para conectar no WhatsApp e esperar até que isto tenha acontecido, para não ocorrer vários erros por parte de ser rápido ou lento.

--- Primeiro/main.py
+++ Primeiro/main.py
@@ -1,36 +1,75 @@
-from selenium import webdriver
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-
+import time
 from datetime import datetime, timedelta
 
 import pyperclip
 
-import time
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Sender:
+    """
+    A classe tem como função armazenar o navegador e as suas funções,
+    para iniciarlizar ela, você irá precisar informar o receptor, ou seja, quem irá
+    receber a mensagem para ser encaminhada para as outras pessoas e a mensagem que
+    será encaminhada, também há outras, como a lista_contatos, que caso você já possua,
+    já poderá ser informada em formato de lista do python e o foi_enviado em formato
+    booleano se caso o receptor já possua a mensagem recebida.
+    """
     service = Service(ChromeDriverManager().install())
     navegador = webdriver.Chrome(service=service)
-    navegador.get("https://web.whatsapp.com/")
 
     def __init__(self, receptor: str,
                  mensagem: str,
                  lista_contatos: list = None,
-                 foi_enviado: bool = False):
+                 foi_enviado: bool = False,
+                 conectado: bool = False):
+
+        self.conectado = conectado
         if lista_contatos is None:
             self.lista_contatos = []
+        else:
+            self.lista_contatos = lista_contatos
         self.receptor = receptor
         self.mensagem = mensagem
         self.foi_enviado = foi_enviado
 
+    # Ligar o WhatsApp, esperando até conectar-se de alguma forma, para continuar.
+    def conectar(self):
+        self.navegador.get("https://web.whatsapp.com/")
+        while 1:
+            try:
+                WebDriverWait(self.navegador, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH,
+                         '//*[@id="app"]/div/div/div[4]/header/div[1]/div/img'))
+                )
+                break
+            except:
+                pass
+        self.conectado = True
+
+    def _se_conectado(self):
+        try:
+            self.navegador.find_element(By.XPATH,
+                                        '//*[@id="app"]/div/div/div[4]/header/div[1]/div/img')
+            if not self.conectado:
+                raise Exception("Conectar-se antes utilizando a função conectar()")
+        except:
+            raise Exception("Conectar-se antes utilizando a função conectar()")
+        time.sleep(2)
+
+    # A função pegar_contatos irá pegar todos os seus contatos da sua lista de contatos
+    # guardando eles numa lista python.
     def pegar_contatos(self):
-        time.sleep(15)
+        self._se_conectado()
         # Ir para a lista de contatos.
         self.navegador.find_element(By.XPATH,
                                     '//*[@id="app"]/div/div/div[4]/header/div[2]/div/span/div[4]/div/span'
@@ -44,40 +83,64 @@ class Sender:
         self._mover_pagina(pagina=origin, delta_y=1000000)
 
         # Pegar os contatos
-        lista = []
-        contatos = self.navegador.find_elements(By.CLASS_NAME, '_30scZ')
-        for contato in contatos:
-            if contato.text not in lista:
-                lista.append(contato.text)
+        self._pegar_contatos()
+
         origin = self.navegador.find_element(By.XPATH,
                                              '//*[@id="app"]/div/div/div[3]/div[1]/span/div/span/div/div[2]')
         quebrar = False
         while 1:
             # Subir a página
-            delta_y = -(origin.rect['y'])
-            self._mover_pagina(pagina=origin, delta_y=(delta_y * 2))
+            delta_y = int(-(1.7 * origin.rect['y']))
+            self._mover_pagina(pagina=origin, delta_y=delta_y)
             # Pegar os contatos atuais mostrando
-            contatos = self.navegador.find_elements(By.CLASS_NAME, '_30scZ')
-            for contato in contatos:
-                if contato.text not in lista:
-                    lista.append(contato.text)
+            contatos = self._pegar_contatos()
 
             # Pegar os separadores para poder ver se acabou os contatos
+            quebrar = self._verificar_quebra(contatos, origin, quebrar)
+            # Terminar o while
+            if quebrar:
+                break
+        return self.lista_contatos
+
+    def _pegar_contatos(self):
+        try:
+            contatos = self.navegador.find_elements(By.CLASS_NAME, '_30scZ')
+            for contato in contatos:
+                if contato.text not in self.lista_contatos:
+                    self.lista_contatos.append(contato.text)
+        except:
+            time.sleep(0.5)
+            contatos = self.navegador.find_elements(By.CLASS_NAME, '_30scZ')
+            for contato in contatos:
+                if contato.text not in self.lista_contatos:
+                    self.lista_contatos.append(contato.text)
+        return contatos
+
+    def _verificar_quebra(self, contatos, origin, quebrar):
+        try:
             separadores = self.navegador.find_elements(By.CLASS_NAME, '_2a-B5')
             for ele in separadores:
                 if ele.text.lower() == 'contatos no whatsapp':
                     self._mover_pagina(pagina=origin, delta_y=-1000000)
                     time.sleep(0.5)
                     for contato in contatos:
-                        if contato.text not in lista:
-                            lista.append(contato.text)
+                        if contato.text not in self.lista_contatos:
+                            self.lista_contatos.append(contato.text)
                     quebrar = True
                     break
-            # Terminar o while
-            if quebrar:
-                break
-        self.lista_contatos = lista
-        return lista
+        except:
+            time.sleep(0.5)
+            separadores = self.navegador.find_elements(By.CLASS_NAME, '_2a-B5')
+            for ele in separadores:
+                if ele.text.lower() == 'contatos no whatsapp':
+                    self._mover_pagina(pagina=origin, delta_y=-1000000)
+                    time.sleep(0.5)
+                    for contato in contatos:
+                        if contato.text not in self.lista_contatos:
+                            self.lista_contatos.append(contato.text)
+                    quebrar = True
+                    break
+        return quebrar
 
     def _mover_pagina(self, pagina, delta_y):
         scroll_origin = ScrollOrigin.from_element(pagina)
@@ -86,8 +149,10 @@ class Sender:
             .perform()
         time.sleep(0.5)
 
+    # A função enviar_para_o_receptor irá enviar a mensagem para o receptor e tornar o
+    # foi_enviado em True, podendo então encaminhar as mensagens.
     def enviar_para_o_receptor(self):
-        time.sleep(5)
+        self._se_conectado()
         # O recetor seria a pessoa que irá receber a mensagem que deve ser
         # encaminhada para todas as outras a cada 5.
         # Clicar na lupa.
@@ -128,9 +193,13 @@ class Sender:
             blocos = int(qtde_contatos / 5) + 1
         return int(blocos)
 
+    # A função encaminhar_a_mensagem irá encaminhar a mensagem para todos da lista_contatos,
+    # num intervalo a cada 5 para não haver bloqueio por parte do WhatsApp.
     def encaminhar_a_mensagem(self):
+        if str(type(self.lista_contatos)) == "<class 'dict'>":
+            raise Exception("Utilizar o encaminhar_a_mensagem_com_data()")
+        self._se_conectado()
         if self.foi_enviado:
-            time.sleep(5)
             for i in range(self._verificar_quantidade_de_envios()):
                 # Rodar o codigo de encaminhar
                 i_inicial = i * 5
@@ -184,6 +253,18 @@ class Sender:
 
 
 class SenderWithDate(Sender):
+    """
+    Com as mesmas funções da classe Sender, porém com extras, sendo elas:\n
+    eng > Que seria a tradução dos dias da semana para o inglês.\n
+    weekdays > Diferenciação entre o dia da semana 0 Segunda, 6 Domingo para poder
+    transformar os dias da semana em data normal.\n
+    lista_contatos > Lista de contatos, porém em forma de dicionário python, sendo
+    o nome da pessoa referindo-se a data dá última mensagem recebida.\n
+    desde > Uma data em formato padrão 00/00/0000, exemplo 07/05/2023, sendo a data primária.\n
+    ate > Uma data em formato padrão 00/00/0000, exemplo 09/09/2023, sendo a data final.\n
+    Com ambos valores acima, usando o exemplo, todos na lista_contatos que enviar mensagem
+    entre o desde(começo) e o ate(fim), 07/05/2023 até 09/09/2023, receberão a mensagem.
+    """
     eng = {"Segunda-Feira": "Monday", "Terça-Feira": "Tuesday", "Quarta-Feira": "Wednesday",
            "Quinta-Feira": "Thursday", "Sexta-Feira": "Friday", "Sábado": "Saturday", "Domingo": "Sunday"}
     weekdays = {0: {'Sunday': -1, 'Saturday': -2, 'Friday': -3, 'Thursday': -4, 'Wednesday': -5, 'Tuesday': -6},
@@ -195,18 +276,17 @@ class SenderWithDate(Sender):
                 6: {'Saturday': -1, 'Friday': -2, 'Thursday': -3, 'Wednesday': -4, 'Tuesday': -5, 'Monday': -6}
                 }
 
-    def __init__(self, receptor: str,
-                 mensagem: str,
-                 lista_contatos: dict = None,
-                 foi_enviado: bool = False,
-                 desde: str = '00/00/0001',
-                 ate=datetime.now().strftime('%d/%m/%Y')):
+    def __init__(self, receptor: str, mensagem: str, conectado: bool = False, lista_contatos: dict = None,
+                 foi_enviado: bool = False, desde: str = '01/01/0001', ate=datetime.now().strftime('%d/%m/%Y')):
 
-        super().__init__(receptor, mensagem, lista_contatos, foi_enviado)
+        super().__init__(receptor, mensagem, lista_contatos, foi_enviado, conectado)
+        self.conectado = conectado
         self.desde = desde
         self.ate = ate
         if lista_contatos is None:
             self.lista_contatos = {}
+        else:
+            self.lista_contatos = lista_contatos
 
     def _converter_data(self, data):
         if data == 'Ontem':
@@ -245,8 +325,10 @@ class SenderWithDate(Sender):
                 ultimo = chave
                 return ultimo
 
+    # Pega os contatos que enviaram mensagem para você e a data da última mensagem,
+    # salvando em formato dicionário python, sendo nome: data
     def pegar_contatos_recentes(self):
-        time.sleep(15)
+        self._se_conectado()
         ultimo = self._pegar_ultimo()
         time.sleep(1)
 
@@ -256,53 +338,59 @@ class SenderWithDate(Sender):
         time.sleep(0.5)
 
         # Pegar os contatos e a data.
-        contatos = self.navegador.find_elements(By.CLASS_NAME, '_8nE1Y')
-        contatos_recentes = {}
-        for contato in contatos:
-            nome = contato.find_element(By.CLASS_NAME, "_11JPr").text
-            data = self._converter_data(contato.find_element(By.CLASS_NAME, "aprpv14t").text)
-            contatos_recentes[nome] = data
+        self._pegar_contatos_com_data()
 
         quebrar = False
         while 1:
             # Descer a página
-            delta_y = int(1.5 * (origin.rect['y']))
+            delta_y = int(1.7 * (origin.rect['y']))
             self._mover_pagina(pagina=origin, delta_y=delta_y)
-            contatos = self.navegador.find_elements(By.CLASS_NAME, '_8nE1Y')
-            try:
-                for contato in contatos:
-                    nome = contato.find_element(By.CLASS_NAME, "_11JPr").text
-                    data = self._converter_data(contato.find_element(By.CLASS_NAME, "aprpv14t").text)
-                    contatos_recentes[nome] = data
-            except:
-                time.sleep(0.2)
-            if ultimo in contatos_recentes:
-                self._mover_pagina(pagina=origin, delta_y=1000000)
+            self._pegar_contatos_com_data()
 
-                contatos = self.navegador.find_elements(By.CLASS_NAME, '_8nE1Y')
-                for contato in contatos:
-                    nome = contato.find_element(By.CLASS_NAME, "_11JPr").text
-                    data = self._converter_data(contato.find_element(By.CLASS_NAME, "aprpv14t").text)
-                    contatos_recentes[nome] = data
-                quebrar = True
+            quebrar = self._verificar_ultimo(origin, quebrar, ultimo)
             if quebrar:
                 break
 
-        self.lista_contatos = contatos_recentes
-        return contatos_recentes
+        return self.lista_contatos
+
+    def _pegar_contatos_com_data(self):
+        try:
+            contatos = self.navegador.find_elements(By.CLASS_NAME, '_8nE1Y')
+            for contato in contatos:
+                nome = contato.find_element(By.CLASS_NAME, "_11JPr").text
+                data = self._converter_data(contato.find_element(By.CLASS_NAME, "aprpv14t").text)
+                self.lista_contatos[nome] = data
+        except:
+            time.sleep(0.5)
+            contatos = self.navegador.find_elements(By.CLASS_NAME, '_8nE1Y')
+            for contato in contatos:
+                nome = contato.find_element(By.CLASS_NAME, "_11JPr").text
+                data = self._converter_data(contato.find_element(By.CLASS_NAME, "aprpv14t").text)
+                self.lista_contatos[nome] = data
+
+    def _verificar_ultimo(self, origin, quebrar, ultimo):
+        if ultimo in self.lista_contatos:
+            self._mover_pagina(pagina=origin, delta_y=1000000)
+            self._pegar_contatos_com_data()
+            quebrar = True
+        return quebrar
 
     # Encaminhará a mensagem para os contados entre as datas entregues através
     # dos elementos "desde" e "ate", exemplo:
     # desde = 01/11/2023 ate = 06/11/2023, todos os contatos que enviaram mensagem
     # entre 01/11/2023 e 06/11/2023 irão receber o encaminhamento da mensagem.
     def encaminhar_a_mensagem_com_data(self):
+        if str(type(self.lista_contatos)) == "<class 'list'>":
+            raise Exception("Utilizar o encaminhar_a_mensagem")
+        self._se_conectado()
         if self.foi_enviado:
             time.sleep(5)
             lista_atualizada = []
             for chave in self.lista_contatos.keys():
                 desde = datetime.strptime(self.desde, '%d/%m/%Y')
                 ate = datetime.strptime(self.ate, '%d/%m/%Y')
-                if desde <= self.lista_contatos[chave] <= ate:
+                transformar = datetime.strptime(self.lista_contatos[chave], '%d/%m/%Y')
+                if desde <= transformar <= ate:
                     lista_atualizada.append(chave)
             if not lista_atualizada == []:
                 for i in range(self._verificar_quantidade_de_envios()):
@@ -355,4 +443,3 @@ class SenderWithDate(Sender):
                     time.sleep(3)
         else:
             return "Envie para o receptor primeiro, caso contrário, ocorrerá um erro."
-
--- Primeiro/test.py
+++ Primeiro/test.py
@@ -1,7 +1,3 @@
-from datetime import datetime, timedelta
+teste = []
 
-A = datetime.strptime("09/05/2019", '%d/%m/%Y')
-B = datetime.strptime("01/10/2020", '%d/%m/%Y')
-lista = [A, B]
-
-print(min(lista))
+print(str(type(teste)))
